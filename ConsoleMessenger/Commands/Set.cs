0a51e5960d5353bee1982d78b44a8887fabc6368
Alexander "Ace" Olofsson <ace@haxalot.com>
2016-10-20 23:04:38 +0200

    Starting to implement the setting system

--- ConsoleMessenger/Commands/Set.cs
+++ ConsoleMessenger/Commands/Set.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ConsoleMessenger.Settings;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +12,25 @@ namespace ConsoleMessenger.Commands
 	[Command("set", Description = "Reads or sets the value of a configurable setting.")]
 	class Set : Command
 	{
+		class PropertyKV
+		{
+			public object Class;
+			public PropertyInfo Property;
+		}
+
 		public void Call()
 		{
-			// TODO: Get all settings for the current context
+			var settings = typeof(Application).GetProperties(BindingFlags.Static | BindingFlags.Public).Where(p => p.GetCustomAttribute<SettingAttribute>() != null).Select(p => new PropertyKV{ Property = p, Class = null });
+
+			if (Application.CurrentChannelBuffer != null)
+				settings = settings.Concat(Application.CurrentChannelBuffer.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(p => p.GetCustomAttribute<SettingAttribute>() != null).Select( p=> new PropertyKV{ Class = Application.CurrentChannelBuffer, Property = p }));
+
+			Debug.WriteLine("Available settings:");
+			foreach (var prop in settings)
+			{
+				var set = prop.Property.GetCustomAttribute<SettingAttribute>();
+				Debug.WriteLine($"- {set.Name} = {prop.Property.GetMethod.Invoke(prop.Class, null)} - {set.Description}");
+			}	
 		}
 
 		public void Call(string name)
