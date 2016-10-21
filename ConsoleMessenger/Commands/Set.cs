0a59e678cdfb34d67d303bce84c6874d6aa2116d
Alexander "Ace" Olofsson <ace@haxalot.com>
2016-10-22 01:20:25 +0200

    First setting system

--- ConsoleMessenger/Commands/Set.cs
+++ ConsoleMessenger/Commands/Set.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace ConsoleMessenger.Commands
 {
@@ -18,15 +19,23 @@ namespace ConsoleMessenger.Commands
 			public PropertyInfo Property;
 		}
 
+        IEnumerable<PropertyKV> _AvailableSettings
+        {
+            get
+            {
+                var settings = typeof(Application).GetProperties(BindingFlags.Static | BindingFlags.Public).Where(p => p.GetCustomAttribute<SettingAttribute>() != null).Select(p => new PropertyKV { Property = p, Class = null });
+
+                if (Application.CurrentChannelBuffer != null)
+                    settings = settings.Concat(Application.CurrentChannelBuffer.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(p => p.GetCustomAttribute<SettingAttribute>() != null).Select(p => new PropertyKV { Class = Application.CurrentChannelBuffer, Property = p }));
+
+                return settings;
+            }
+        }
+
 		public void Call()
 		{
-			var settings = typeof(Application).GetProperties(BindingFlags.Static | BindingFlags.Public).Where(p => p.GetCustomAttribute<SettingAttribute>() != null).Select(p => new PropertyKV{ Property = p, Class = null });
-
-			if (Application.CurrentChannelBuffer != null)
-				settings = settings.Concat(Application.CurrentChannelBuffer.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(p => p.GetCustomAttribute<SettingAttribute>() != null).Select( p=> new PropertyKV{ Class = Application.CurrentChannelBuffer, Property = p }));
-
 			Debug.WriteLine("Available settings:");
-			foreach (var prop in settings)
+			foreach (var prop in _AvailableSettings)
 			{
 				var set = prop.Property.GetCustomAttribute<SettingAttribute>();
 				Debug.WriteLine($"- {set.Name} = {prop.Property.GetMethod.Invoke(prop.Class, null)} - {set.Description}");
@@ -35,12 +44,31 @@ namespace ConsoleMessenger.Commands
 
 		public void Call(string name)
 		{
-			// TODO: Get the value of the given setting
+            var setting = _AvailableSettings.FirstOrDefault(s => s.Property.GetCustomAttribute<SettingAttribute>().Name.Equals(name, StringComparison.CurrentCultureIgnoreCase));
+
+            if (setting == null)
+                Debug.WriteLine("No such setting {0}", name);
+            else
+            {
+                var att = setting.Property.GetCustomAttribute<SettingAttribute>();
+                Debug.WriteLine($"{att.Name} = {setting.Property.GetValue(setting.Class)} - {att.Description}");
+            }
 		}
 
 		public void Call(string name, params string[] values)
 		{
-			// TODO: Set a setting
+            var setting = _AvailableSettings.FirstOrDefault(s => s.Property.GetCustomAttribute<SettingAttribute>().Name.Equals(name, StringComparison.CurrentCultureIgnoreCase));
+
+            if (setting == null)
+                Debug.WriteLine("No such setting {0}", name);
+            else
+            {
+                var att = setting.Property.GetCustomAttribute<SettingAttribute>();
+                var conv = TypeDescriptor.GetConverter(setting.Property.PropertyType);
+
+                setting.Property.SetValue(setting.Class, conv.ConvertFromString(values.ToString(" ")));
+                Debug.WriteLine($"{att.Name} = {setting.Property.GetValue(setting.Class)} - {att.Description}");
+            }
 		}
 	}
 }
