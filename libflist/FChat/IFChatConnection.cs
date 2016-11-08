cbdc85e71b2f0c20b0987ae29477c0e034032a4d
Alexander "Ace" Olofsson <ace@haxalot.com>
2016-11-08 06:22:59 +0100

    Need to decide what should be in the interface

--- libflist/FChat/IFChatConnection.cs
+++ libflist/FChat/IFChatConnection.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using libflist.Events;
+using libflist.FChat.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,85 @@ namespace libflist.FChat
 {
 	interface IFChatConnection
 	{
+		// Server events
+		event EventHandler OnConnected;
+		event EventHandler OnDisconnected;
+		event EventHandler OnIdentified;
+		event EventHandler<ServerVariableEventArgs> OnServerVariableUpdate;
 
+		// Message events
+		event EventHandler<ErrorEventArgs> OnError;
+		event EventHandler<CommandEventArgs> OnErrorMessage;
+		event EventHandler<CommandEventArgs> OnRawMessage;
+		event EventHandler<CommandEventArgs> OnSYSMessage;
+		event EventHandler<CommandEventArgs> OnSendMessage;
+
+		// Channel list events
+		event EventHandler OnOfficialListUpdate;
+		event EventHandler OnPrivateListUpdate;
+
+		// OP events
+		event EventHandler<CharacterEntryEventArgs> OnOPAdded;
+		event EventHandler<CharacterEntryEventArgs> OnOPRemoved;
+
+		// Channel entry events
+		event EventHandler<ChannelEntryEventArgs> OnChannelJoin;
+		event EventHandler<ChannelEntryEventArgs> OnChannelLeave;
+
+		// Channel user entry events
+		event EventHandler<ChannelUserEntryEventArgs> OnChannelUserJoin;
+		event EventHandler<ChannelUserEntryEventArgs> OnChannelUserLeave;
+
+		// Channel admin events
+		event EventHandler<ChannelAdminActionEventArgs> OnChannelUserKicked;
+		event EventHandler<ChannelAdminActionEventArgs> OnChannelUserBanned;
+		event EventHandler<ChannelAdminActionEventArgs> OnChannelUserUnbanned;
+		event EventHandler<ChannelAdminActionEventArgs> OnChannelUserTimedout;
+
+		// Channel status events
+		event EventHandler<ChannelEntryEventArgs<string>> OnChannelDescriptionChange;
+		event EventHandler<ChannelEntryEventArgs<ChannelMode>> OnChannelModeChange;
+		event EventHandler<ChannelEntryEventArgs<Character>> OnChannelOwnerChange;
+		event EventHandler<ChannelEntryEventArgs<ChannelStatus>> OnChannelStatusChange;
+		// event EventHandler<ChannelEntryEventArgs<string>> OnChannelTitleChange;
+
+		// Channel OP events
+		event EventHandler<ChannelUserEntryEventArgs> OnChannelOPAdded;
+		event EventHandler<ChannelUserEntryEventArgs> OnChannelOPRemoved;
+
+		// Channel message events
+		event EventHandler<ChannelUserMessageEventArgs> OnChannelChatMessage;
+		event EventHandler<ChannelUserMessageEventArgs> OnChannelLFRPMessage;
+		event EventHandler<ChannelUserMessageEventArgs> OnChannelRollMessage;
+		event EventHandler<ChannelEntryEventArgs<string>> OnChannelSYSMessage;
+
+		// Character entry events
+		event EventHandler<CharacterEntryEventArgs> OnCharacterOnline;
+		event EventHandler<CharacterEntryEventArgs> OnCharacterOffline;
+
+		// Character list events
+		event EventHandler OnFriendsListUpdate;
+		event EventHandler OnIgnoreListUpdate;
+
+		// Character admin events
+		event EventHandler<AdminActionEventArgs> OnCharacterKicked;
+		event EventHandler<AdminActionEventArgs> OnCharacterBanned;
+		event EventHandler<AdminActionEventArgs> OnCharacterUnbanned;
+		event EventHandler<AdminActionEventArgs> OnCharacterTimedout;
+
+		// Character status events
+		event EventHandler<CharacterEntryEventArgs<CharacterStatus>> OnCharacterStatusChange;
+		event EventHandler<CharacterEntryEventArgs<TypingStatus>> OnCharacterTypingChange;
+
+		// Character message events
+		event EventHandler<CharacterMessageEventArgs> OnCharacterChatMessage;
+
+
+		void AquireTicket(string User, string Password);
+		void Connect();
+		// void Connect(string User, string Password/APIKey);
+		void Disconnect();
+		void Reconnect(bool AutoLogin = true);
+		void Login(string Character);
 	}
 }
